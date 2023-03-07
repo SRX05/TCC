8612b3d13996e3ab04bd2bd60af9c1967f76c453
Sabrynne Rodrigues <105216097+SRX05@users.noreply.github.com>
2023-03-07 10:18:31 -0300

    Add files via upload

---
+++ TCC/js/calendario.js
@@ -0,0 +1,27 @@
+
+  // obter a data atual
+  const today = new Date();
+
+  // obter o elemento h2 que exibe o mês e ano atual
+  const monthElement = document.querySelector('.month h2');
+
+  // obter o elemento ul que contém os dias do mês
+  const daysElement = document.querySelector('.days');
+
+  // definir um array com o nome dos meses
+  const months = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro'
+  ];
+
+  // definir o nome do mês e ano atual
